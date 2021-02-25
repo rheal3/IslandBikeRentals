bb7c322e81e21d06c346cd4fde454e4dabd1d3e2
Rebekah <heal.rebekah@gmail.com>
2021-02-25 13:19:26 +1000

    changed DB_URI to DATABASE_URL

--- src/settings.py
+++ src/settings.py
@@ -5,10 +5,10 @@ class Config(object):
 
     @property
     def SQLALCHEMY_DATABASE_URI(self):
-        value = os.getenv("DB_URI")
+        value = os.getenv("DATABASE_URL")
 
         if not value:
-            raise ValueError("DB_URI is not set!")
+            raise ValueError("DATABASE_URL is not set!")
         
         return value
 
