3c64343aaace5d0b9f5202f5327d05a780455323
Rebekah <heal.rebekah@gmail.com>
2021-03-02 17:34:49 +1000

    delete booking

--- src/controllers/bookings_controller.py
+++ src/controllers/bookings_controller.py
@@ -25,27 +25,23 @@ def booking_show(id):
     """
     booking = Booking.query.get(id)
     payment = Payment.query.filter_by(booking_id=id).first()
-
-    # if request.method == "POST":
-    #     booking.rental_complete = request.form.get("rental_complete")
-    #     db.session.commit()
-    #     return redirect(url_for("bookings.booking_show"), id=id)
-
     return render_template("bookings_single.html", booking=booking, payment=payment)
 
-@bookings.route("/delete/<int:id>", methods=["DELETE"])
+@bookings.route("/<int:id>/delete", methods=["GET", "DELETE"])
 def booking_delete(id):
     """
     Delete data for single booking using id.
     """
     booking = Booking.query.get(id)
+    payment = Payment.query.filter_by(booking_id=id).first()
     if not booking:
         return "DELETED"
     db.session.delete(booking)
+    db.session.delete(payment)
     db.session.commit()
-    return jsonify(booking_schema.dump(booking))
+    return redirect(url_for('bookings.booking_index'))
 
-@bookings.route("/update/<int:id>", methods=["POST", "GET"])
+@bookings.route("/<int:id>/update", methods=["POST", "GET"])
 def booking_update(id):
     """
     Update booking using id.
