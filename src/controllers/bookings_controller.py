49ad614aee1cbf936ef3ae47ff5ad30bf29ca99a
Rebekah <heal.rebekah@gmail.com>
2021-03-02 17:22:45 +1000

    created booking update functionality

--- src/controllers/bookings_controller.py
+++ src/controllers/bookings_controller.py
@@ -3,7 +3,7 @@ from models.Payment import Payment
 from schemas.BookingSchema import booking_schema, bookings_schema
 from schemas.PaymentSchema import payment_schema, payments_schema
 from main import db
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for
 from sqlalchemy.orm import joinedload
 
 
@@ -18,14 +18,19 @@ def booking_index():
     payments = Payment.query.all()
     return render_template("bookings_index.html", bookings=bookings, payments=payments)    
 
-@bookings.route("/<int:id>", methods=["GET"])
+@bookings.route("/<int:id>", methods=["POST", "GET"])
 def booking_show(id):
     """
     Show data for single booking using id.
     """
     booking = Booking.query.get(id)
     payment = Payment.query.filter_by(booking_id=id).first()
-    # return jsonify(booking_schema.dump(booking))
+
+    # if request.method == "POST":
+    #     booking.rental_complete = request.form.get("rental_complete")
+    #     db.session.commit()
+    #     return redirect(url_for("bookings.booking_show"), id=id)
+
     return render_template("bookings_single.html", booking=booking, payment=payment)
 
 @bookings.route("/delete/<int:id>", methods=["DELETE"])
@@ -45,13 +50,38 @@ def booking_update(id):
     """
     Update booking using id.
     """
-    pass
+    if request.method == "POST":
+        month = request.form.get("month")
+        day = request.form.get("day")
+        year = request.form.get("year")
 
-    # if request.method == "POST":
-    #     booking = Booking.query.filter_by(id=id)
-    #     booking_fields = booking_schema.load(request.json)
-    #     booking.update(booking_fields)
-    #     db.session.commit()
-        # return jsonify(booking_schema.dump(booking[0]))
-        # return template
-    # return template
+        booking = Booking.query.filter_by(id=id).first()
+        booking.first_name = request.form.get("first_name")
+        booking.last_name = request.form.get("last_name")
+        booking.phone = request.form.get("phone")
+        booking.email = request.form.get("email")
+        booking.num_participants = request.form.get("num_participants")
+        booking.booking_date = f"{year}-{month}-{day}"
+        booking.collection_time = request.form.get("collection_time")
+        booking.return_time = request.form.get("return_time")
+
+        if request.form.get("rental_complete") == True:
+            booking.rental_complete = True
+        else:
+            booking.rental_complete = False
+
+        payment = Payment.query.filter_by(booking_id=id).first()
+        payment.full_amount_due = float(request.form.get("full_amount_due"))
+
+        if request.form.get("remainder_due_check") == "paid":
+            payment.remainder_due = 0
+        else:
+            payment.remainder_due = request.form.get("remainder_due")
+
+        db.session.commit()
+
+        return redirect(url_for("bookings.booking_index"))
+
+    booking = Booking.query.get(id)
+    payment = Payment.query.filter_by(booking_id=id).first()
+    return render_template("booking_update.html", booking=booking, payment=payment)
